d65877ca5f75c24e9a8eea2273379bf2c566d900
Anton Stsefanovich <stefanovichanton1@gmail.com>
2023-02-21 14:56:37 -0800

    Corrected

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         task1();
@@ -13,33 +15,81 @@ public class Main {
         massive1[0] = 1;
         massive1[1] = 2;
         massive1[2] = 3;
-        int firstMassive = massive1[0];
-        int secondMassive = massive1[1];
-        int thirdMassive = massive1[2];
         double[] massive2 = {1.57, 7.654, 9.986};
         int[] massive3 = new int[12];
-        System.out.println(massive1[1]);
+
+
     }
+
 
     public static void task2() {
         System.out.println("Задача 2");
         int[] massive1 = new int[3];
+        massive1[0] = 1;
+        massive1[1] = 2;
+        massive1[2] = 3;
         double[] massive2 = {1.57, 7.654, 9.986};
         int[] massive3 = new int[2];
-        System.out.println(massive1[0] + ", " + massive1[1] + ", " + massive1[2]);
-        System.out.println(massive2[0] + ", " + massive2[1] + ", " + massive2[2]);
-        System.out.println(massive3[0] + ", " + massive3[1]);
+        System.out.println("Первый массив");
+        for (int i = 0; i < massive1.length; i++) {
+            System.out.print(massive1[i]);
+            if (i != massive1.length - 1) {
+                System.out.println(", ");
+            }
+        }
+        System.out.println();
+        System.out.println("Второй массив");
+        for (int i = 0; i < massive2.length; i++) {
+            System.out.print(massive2[i]);
+            if (i != massive2.length - 1) {
+                System.out.println(", ");
+            }
+        }
+        System.out.println();
+        System.out.println("Третий массив");
+        for (int i = 0; i < massive3.length; i++) {
+            System.out.print(massive3[i]);
+            if (i != massive3.length - 1) {
+                System.out.println(", ");
+            }
+        }
+        System.out.println();
     }
 
     public static void task3() {
         System.out.println("Задача 3");
         int[] massive1 = new int[3];
+        massive1[0] = 1;
+        massive1[1] = 2;
+        massive1[2] = 3;
         double[] massive2 = {1.57, 7.654, 9.986};
         int[] massive3 = new int[2];
-        System.out.println(massive1[2] + ", " + massive1[1] + ", " + massive1[0]);
-        System.out.println(massive2[2] + ", " + massive2[1] + ", " + massive2[0]);
-        System.out.println(massive3[1] + ", " + massive3[0]);
+        System.out.println("Первый массив");
+        for (int i = massive1.length - 1; i >= 0; i--) {
+            System.out.print(massive1[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        System.out.println("Второй массив");
+        for (int i = massive2.length - 1; i >= 0; i--) {
+            System.out.print(massive2[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        System.out.println("Третий массив");
+        for (int i = massive3.length - 1; i >= 0; i--) {
+            System.out.print(massive3[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
     }
+
 
     public static void task4() {
         System.out.println("Задача 4");
@@ -47,21 +97,18 @@ public class Main {
         massive1[0] = 1;
         massive1[1] = 2;
         massive1[2] = 3;
-        int firstMassive = massive1[0];
-        int secondMassive = massive1[1];
-        int thirdMassive = massive1[2];
         double[] massive2 = {1.57, 7.654, 9.986};
         int[] massive3 = {3, 5, 6, 7, 8, 9};
         for (int i = 0; i < massive3.length; i++) {
             if (massive3[i] % 2 == 1) {
                 massive3[i]++;
 
-                System.out.println(massive3[i] + " ");
-
 
             }
 
+
         }
+        System.out.println(Arrays.toString(massive3));
     }
 }
 
